6b2b4ee04bf9c1faa83754fc629aa701473ac787
IgorGanapolsky <eazyigz@gmail.com>
2015-08-25 17:32:01 -0400

    Create gradle project. Include dependencies. Update to latest android support lib.

---
+++ app/src/main/java/com.swanson.octodroid/GitHubService.java
@@ -0,0 +1,14 @@
+package com.swanson.octodroid;
+
+import retrofit.RestAdapter;
+
+public class GitHubService {
+    private static String API_URL = "https://api.github.com";
+
+    public static GitHub getService() {
+        return new RestAdapter.Builder()
+                .setEndpoint(API_URL)
+                .build()
+                .create(GitHub.class);
+    }
+}
